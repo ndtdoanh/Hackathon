a6762c7295079c43a4a779067618c8edfe5061da
doanh <nguyentrongdoanh202@gmail.com>
2025-01-02 03:19:51 +0700

    initial commit

---
+++ src/main/java/com/ndtdoanh/hackathon/HackathonApplication.java
@@ -0,0 +1,14 @@
+package com.ndtdoanh.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonApplication.class, args);
+		System.out.println("Hello, Hackathon Application has started successfully!");
+	}
+
+}
