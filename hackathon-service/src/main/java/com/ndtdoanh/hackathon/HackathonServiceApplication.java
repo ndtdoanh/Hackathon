1cf98db808ee3ec9b4785a4cb4e931e1510aef54
doanh <nguyentrongdoanh202@gmail.com>
2025-01-14 07:50:08 +0700

    initial code base microservice

---
+++ hackathon-service/src/main/java/com/ndtdoanh/hackathon/HackathonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ndtdoanh.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonServiceApplication.class, args);
+	}
+
+}
