52e966be32040604d619ebea9c4edb58efd0864e
doanh <nguyentrongdoanh202@gmail.com>
2025-01-14 07:50:08 +0700

    initial code base microservice

---
+++ hackathon-service/src/test/java/com/ndtdoanh/hackathon/HackathonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndtdoanh.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
