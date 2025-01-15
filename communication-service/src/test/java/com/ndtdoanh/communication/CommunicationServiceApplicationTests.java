acab24ff5eeaa37cd501ac6ad71e775a1a517330
doanh <nguyentrongdoanh202@gmail.com>
2025-01-16 03:52:30 +0700

    update code base

--- communication-service/src/test/java/com/ndtdoanh/communication/CommunicationServiceApplicationTests.java
+++ communication-service/src/test/java/com/ndtdoanh/communication/CommunicationServiceApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CommunicationServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
--- hackathon-service/src/main/java/com/ndtdoanh/hackathon/HackathonServiceApplication.java
+++ hackathon-service/src/main/java/com/ndtdoanh/hackathon/HackathonServiceApplication.java
@@ -2,12 +2,13 @@ package com.ndtdoanh.hackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HackathonServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HackathonServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonServiceApplication.class, args);
+    }
 }
