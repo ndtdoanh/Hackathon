2bc2870bc1b721cb972ca852ed555c5657e27cd2
KienNguyenFPT <kiennctqe170207@fpt.edu.vn>
2025-02-16 15:58:46 +0700

    refactor format of datatype - RequestDateTime has pattern - yyy-MM-dd'T'HH:mm:ss

--- hackathon-service/src/main/java/com/hacof/hackathon/util/CustomLocalDateTimeDeserialized.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/util/CustomLocalDateTimeDeserialized.java
@@ -3,28 +3,19 @@ package com.hacof.hackathon.util;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
 public class CustomLocalDateTimeDeserialized extends JsonDeserializer<LocalDateTime> {
-    private static final DateTimeFormatter formatter =
-            DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS"); // Without trailing fractions
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
     @Override
-    public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+    public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt)
+            throws IOException, JsonProcessingException {
         String date = p.getText();
-        try {
-            // Preprocess the input string to remove the extra period and fractional parts
-            if (date.contains(".")) {
-                date = date.substring(0, date.indexOf(".")); // Take only the valid part before the first '.'
-            }
-            // Parse the cleaned-up string
-            return LocalDateTime.parse(date, formatter);
-        } catch (DateTimeParseException e) {
-            throw new IOException("Invalid date format: " + date, e);
-        }
+        return LocalDateTime.parse(date, formatter);
     }
 }
