72ea01be689e3908372053599d43d9069c897aae
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-02-27 16:17:32 +0700

    Refactor campusDTO - Not use Path variable

--- hackathon-service/src/main/java/com/hacof/hackathon/dto/CampusDTO.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/dto/CampusDTO.java
@@ -5,33 +5,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class CampusDTO {
     private Long id;
 
-    @NotBlank(message = "Name is mandatory")
+    @NotNull(message = "Name is mandatory")
     private String name;
 
-    @NotBlank(message = "Location is mandatory")
+    @NotBlank(message = "Invalid location for the campus")
     private String location;
 
-    @NotBlank(message = "createBy is mandatory")
+    @NotNull(message = "Created by is mandatory")
     private String createdBy;
 
     private LocalDateTime createdDate;
 
-    @NotBlank(message = "lastModifiedBy is mandatory")
+    @NotNull(message = "Last modified by is mandatory")
     private String lastModifiedBy;
 
     private LocalDateTime lastModifiedDate;
 
+    @JsonIgnore
     private List<EventDTO> events = new ArrayList<>();
+
+    @JsonIgnore
     private List<MentorDTO> mentors = new ArrayList<>();
+
+    @JsonIgnore
     private List<TrainingSessionDTO> trainingSessions = new ArrayList<>();
 }
