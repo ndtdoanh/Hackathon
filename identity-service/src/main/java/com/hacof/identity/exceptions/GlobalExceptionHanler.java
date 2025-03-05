86ec9874d4e91d7053c44113a6e8fbf8b668a206
doanh <nguyentrongdoanh202@gmail.com>
2025-03-05 21:16:11 +0700

    swagger identity

--- identity-service/src/main/java/com/hacof/identity/exceptions/GlobalExceptionHanler.java
+++ identity-service/src/main/java/com/hacof/identity/exceptions/GlobalExceptionHanler.java
@@ -10,30 +10,30 @@ import com.hacof.identity.dtos.ApiResponse;
 @ControllerAdvice
 public class GlobalExceptionHanler {
 
-    @ExceptionHandler(value = Exception.class)
-    ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException exception) {
-
-        ApiResponse apiResponse = new ApiResponse();
-
-        apiResponse.setCode(ErrorCode.UNCATEGORIZED.getCode());
-        apiResponse.setMessage(ErrorCode.UNCATEGORIZED.getMessage());
+    @ExceptionHandler(value = RuntimeException.class)
+    ResponseEntity<ApiResponse<?>> handlingRuntimeException(RuntimeException exception) {
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .code(ErrorCode.UNCATEGORIZED.getCode())
+                .message(exception.getMessage())
+                .build();
 
         return ResponseEntity.badRequest().body(apiResponse);
     }
 
     @ExceptionHandler(value = AppException.class)
-    ResponseEntity<ApiResponse> handlingAppException(AppException exception) {
+    ResponseEntity<ApiResponse<?>> handlingAppException(AppException exception) {
         ErrorCode errorCode = exception.getErrorCode();
-        ApiResponse apiResponse = new ApiResponse();
 
-        apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(errorCode.getMessage());
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
 
         return ResponseEntity.status(errorCode.getStatusCode()).body(apiResponse);
     }
 
     @ExceptionHandler(value = AccessDeniedException.class)
-    ResponseEntity<ApiResponse> handlingAccessDeniedException(AccessDeniedException exception) {
+    ResponseEntity<ApiResponse<?>> handlingAccessDeniedException(AccessDeniedException exception) {
         ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
 
         return ResponseEntity.status(errorCode.getStatusCode())
