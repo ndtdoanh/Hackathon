b2272740a602beb0cd476f8371f3559e8430d286
lqhuy28 <lequanghuy2809@gmail.com>
2025-03-08 10:07:23 +0700

    Upload file with firebase

---
+++ submission-service/src/main/java/com/hacof/submission/config/FirebaseConfig.java
@@ -0,0 +1,27 @@
+package com.hacof.submission.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseApp initialize() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("D:/Download/Ki8/hacof-7d63e-firebase-adminsdk-fbsvc-b004f29c7f.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://hacof-7d63e.appspot.com")
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+}
