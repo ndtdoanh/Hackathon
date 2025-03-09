d612c57dbe64f1e8f47a5eb03aea0b06b15b60b5
KienNguyenFPT <kiennctqe170207@fpt.edu.vn>
2025-03-09 22:08:37 +0700

    add new DTO

---
+++ hackathon-service/src/main/java/com/hacof/hackathon/dto/TrainingSessionDTO.java
@@ -0,0 +1,10 @@
+package com.hacof.hackathon.dto;
+
+import lombok.Data;
+
+@Data
+public class TrainingSessionDTO {
+    private Long id;
+    private String name;
+    private String description;
+}
