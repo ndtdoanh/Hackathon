38ca55a8cc1e2eaa6bc96214465e4c0bd3358e2b
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-09 22:11:28 +0700

    add setter annotation

--- hackathon-service/src/main/java/com/hacof/hackathon/dto/ResourceDTO.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/dto/ResourceDTO.java
@@ -1,15 +1,32 @@
 package com.hacof.hackathon.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import com.hacof.hackathon.constant.ResourceStatus;
 import com.hacof.hackathon.constant.ResourceType;
 
 import lombok.Data;
+import lombok.Setter;
 
 @Data
+@Setter
 public class ResourceDTO {
     private Long id;
+
+    @NotBlank(message = "Name is mandatory")
     private String name;
+
+    @NotNull(message = "Resource type is mandatory")
     private ResourceType resourceType;
+
+    @NotNull(message = "Status is mandatory")
     private ResourceStatus status;
-    private Long hackathonId;
+
+    @Min(value = 1, message = "Quantity must be at least 1")
+    private int quantity;
+
+    @NotNull(message = "Competition round ID is mandatory")
+    private Long competitionRoundId;
 }
