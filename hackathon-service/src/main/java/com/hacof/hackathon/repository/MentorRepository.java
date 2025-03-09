4e902a8792a503aae360755633fb060393a9b601
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-09 22:11:58 +0700

    add findById method

--- hackathon-service/src/main/java/com/hacof/hackathon/repository/MentorRepository.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/repository/MentorRepository.java
@@ -1,12 +1,7 @@
 package com.hacof.hackathon.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hacof.hackathon.entity.Mentor;
 
-public interface MentorRepository extends JpaRepository<Mentor, Long> {
-    @Override
-    Optional<Mentor> findById(Long aLong);
-}
+public interface MentorRepository extends JpaRepository<Mentor, Long> {}
