1a3de259994ae2c87762d4a01726cd9ea174fea7
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-09 22:07:28 +0700

    add validation annotation

--- hackathon-service/src/main/java/com/hacof/hackathon/dto/HackathonDTO.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/dto/HackathonDTO.java
@@ -1,19 +1,51 @@
 package com.hacof.hackathon.dto;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.hacof.hackathon.util.CustomLocalDateTimeDeserialized;
 
 import lombok.Data;
 
 @Data
 public class HackathonDTO {
     private Long id;
+
+    @NotBlank(message = "Name is mandatory")
     private String name;
+
     private String bannerImageUrl;
     private String description;
-    private Date startDate;
-    private Date endDate;
+
+    @NotNull(message = "Start date is mandatory")
+    @JsonDeserialize(using = CustomLocalDateTimeDeserialized.class)
+    private LocalDateTime startDate;
+
+    @NotNull(message = "Start date is mandatory")
+    @JsonDeserialize(using = CustomLocalDateTimeDeserialized.class)
+    private LocalDateTime endDate;
+
     private Long organizerId;
-    private String organizerName; // New field to include organizer's name
+    private String organizerName;
+    private List<CompetitionRoundDTO> rounds;
+    private List<EventDTO> events;
+    private List<JudgeDTO> judges;
+    // private List<CheckInDTO> checkIns;
+    private List<ResourceDTO> resources;
+
+    @NotBlank(message = "Created by is mandatory")
     private String createdBy;
-    private String lastUpdatedBy;
+
+    @NotNull(message = "Created date is mandatory")
+    private LocalDateTime createdDate;
+
+    @NotBlank(message = "Last modified by is mandatory")
+    private String lastModifiedBy;
+
+    @NotNull(message = "Last modified date is mandatory")
+    private LocalDateTime lastModifiedDate;
 }
