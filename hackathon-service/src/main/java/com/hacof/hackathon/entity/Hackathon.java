d37ab3749bd8726bd26ea37fe035ff5bec734dfb
KienNguyenFPT <kiennctqe170207@fpt.edu.vn>
2025-03-09 22:07:19 +0700

    add validation annotation

--- hackathon-service/src/main/java/com/hacof/hackathon/entity/Hackathon.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/entity/Hackathon.java
@@ -1,17 +1,9 @@
 package com.hacof.hackathon.entity;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,7 +16,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "Hackathons")
-public class Hackathon {
+public class Hackathon extends AuditBase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -32,15 +24,20 @@ public class Hackathon {
     private String name;
     private String bannerImageUrl;
     private String description;
-    private Date startDate;
-    private Date endDate;
-    private Integer maxTeams;
-    private Integer minTeamSize;
-    private Integer maxTeamSize;
+
+    @Column(name = "start_date", columnDefinition = "datetime(6)", nullable = false)
+    private LocalDateTime startDate; // example:  2024-02-16 12:34:56.123456. -> datetime(6)
+
+    @Column(name = "end_date", columnDefinition = "datetime(6)", nullable = false)
+    private LocalDateTime endDate;
+
+    private int maxTeams;
+    private int minTeamSize;
+    private int maxTeamSize;
     private String status;
 
     @ManyToOne
-    @JoinColumn(name = "organizer_id", nullable = false)
+    @JoinColumn(name = "organizer_id")
     private User organizer;
 
     @OneToMany(mappedBy = "hackathon", cascade = CascadeType.ALL, orphanRemoval = true)
