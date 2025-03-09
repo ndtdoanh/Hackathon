5122e1d574d380f16809c2ebe8432ae2d97e9783
KienNguyenFPT <kiennctqe170207@fpt.edu.vn>
2025-03-09 22:12:09 +0700

    add validation annotation

--- hackathon-service/src/main/java/com/hacof/hackathon/dto/MentorDTO.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/dto/MentorDTO.java
@@ -1,10 +1,19 @@
 package com.hacof.hackathon.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class MentorDTO {
     private Long id;
+
+    @NotBlank(message = "Name is mandatory")
     private String name;
+
+    @NotBlank(message = "Expertise is mandatory")
     private String expertise;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
 }
