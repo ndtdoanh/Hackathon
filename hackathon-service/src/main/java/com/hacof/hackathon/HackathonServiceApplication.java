3a11fbe79612911caa4c47898862b0839c6d0cab
KienNguyenFPT <kiennctqe170207@fpt.edu.vn>
2025-03-09 22:07:39 +0700

    refactor code

--- hackathon-service/src/main/java/com/hacof/hackathon/HackathonServiceApplication.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/HackathonServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-
 // @EnableDiscoveryClient
 public class HackathonServiceApplication {
 
