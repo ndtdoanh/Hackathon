671e1e3218e6829d597e563d2c49179f7ba43868
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-13 01:06:08 +0700

    add specification jpa

--- hackathon-service/src/main/java/com/hacof/hackathon/repository/CompetitionRoundRepository.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/repository/CompetitionRoundRepository.java
@@ -1,7 +1,9 @@
 package com.hacof.hackathon.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.hacof.hackathon.entity.CompetitionRound;
 
-public interface CompetitionRoundRepository extends JpaRepository<CompetitionRound, Long> {}
+public interface CompetitionRoundRepository
+        extends JpaRepository<CompetitionRound, Long>, JpaSpecificationExecutor<CompetitionRound> {}
