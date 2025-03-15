16c76fd3c2b5d5db7e77c59b47f24d3ccaecfabe
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-15 19:19:59 +0700

    change @data to @getter, @setter, add AuditBaseDTO

--- hackathon-service/src/main/java/com/hacof/hackathon/dto/EventDTO.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/dto/EventDTO.java
@@ -1,30 +1,17 @@
 package com.hacof.hackathon.dto;
 
-import java.time.LocalDateTime;
-
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-public class EventDTO {
+@Getter
+@Setter
+public class EventDTO extends AuditBaseDTO {
     private Long id;
 
     @NotBlank(message = "Name is mandatory")
     private String name;
 
     private String description;
-
-    @NotBlank(message = "Created by is mandatory")
-    private String createdBy;
-
-    @NotNull(message = "Created date is mandatory")
-    private LocalDateTime createdDate;
-
-    @NotBlank(message = "Last updated by is mandatory")
-    private String lastUpdatedBy;
-
-    @NotNull(message = "Last updated date is mandatory")
-    private LocalDateTime lastUpdatedDate;
 }
