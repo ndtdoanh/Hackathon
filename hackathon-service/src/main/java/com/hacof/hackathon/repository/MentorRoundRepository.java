9226daabb314ce5b5227fa2e4fec212220e26fe8
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-16 23:52:30 +0700

    remove all things that relevant to MENTOR entity

--- hackathon-service/src/main/java/com/hacof/hackathon/repository/MentorRoundRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hacof.hackathon.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hacof.hackathon.entity.MentorRound;
-
-public interface MentorRoundRepository extends JpaRepository<MentorRound, Long> {}
