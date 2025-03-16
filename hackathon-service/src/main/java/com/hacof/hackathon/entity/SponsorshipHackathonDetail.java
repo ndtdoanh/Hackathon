8105a3c983e81b9576df5d668282ed5f65edb608
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-16 21:59:19 +0700

    add sponsorship entity

---
+++ hackathon-service/src/main/java/com/hacof/hackathon/entity/SponsorshipHackathonDetail.java
@@ -0,0 +1,50 @@
+package com.hacof.hackathon.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.hacof.hackathon.constant.SponsorshipDetailStatus;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "sponsorship_hackathon_details")
+public class SponsorshipHackathonDetail extends AuditBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "sponsorship_hackathon_id")
+    SponsorshipHackathon sponsorshipHackathon;
+
+    @Column(name = "money_spent")
+    double moneySpent;
+
+    @Lob
+    @Column(name = "content")
+    String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    SponsorshipDetailStatus status;
+
+    @Column(name = "time_from")
+    LocalDateTime timeFrom;
+
+    @Column(name = "time_to")
+    LocalDateTime timeTo;
+}
