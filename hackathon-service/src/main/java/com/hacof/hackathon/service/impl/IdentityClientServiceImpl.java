0ba8ae211b86b1fcb885ba1b5e7feffad734623d
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-17 02:05:06 +0700

    add IdentityClientServiceImpl

--- hackathon-service/src/main/java/com/hacof/hackathon/service/impl/IdentityClientServiceImpl.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/service/impl/IdentityClientServiceImpl.java
@@ -1,23 +1,20 @@
 package com.hacof.hackathon.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-
-import java.util.List;
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class IdentityClientServiceImpl {
-//    private final IdentityClientService identityClientService;
-//
-//    @GetMapping("/users")
-//    public ApiResponse<List<UserResponse>> getUsers(@RequestHeader("Authorization") String token) {
-//        log.info("Calling identity-service with token: {}", token);
-//        return identityClientService.getUsers(token);
-//    }
+    //    private final IdentityClientService identityClientService;
+    //
+    //    @GetMapping("/users")
+    //    public ApiResponse<List<UserResponse>> getUsers(@RequestHeader("Authorization") String token) {
+    //        log.info("Calling identity-service with token: {}", token);
+    //        return identityClientService.getUsers(token);
+    //    }
 
 }
