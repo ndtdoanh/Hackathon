564498e0355e3cc02d6123f56060336f490308ec
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-16 23:54:07 +0700

    remove teammember entity, team member repository

--- hackathon-service/src/main/java/com/hacof/hackathon/repository/TeamMemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hacof.hackathon.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hacof.hackathon.entity.TeamMember;
-
-public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {}
