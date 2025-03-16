82accb01c14c80f8fd3a2ccdca3c9f9a5c010249
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-16 21:39:22 +0700

    refactor entity

---
+++ hackathon-service/src/main/java/com/hacof/hackathon/entity/Message.java
@@ -0,0 +1,30 @@
+package com.hacof.hackathon.entity;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "messages")
+public class Message extends AuditCreatedBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "conversation_id")
+    Conversation conversation;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    String content;
+
+    @Column(name = "is_deleted")
+    boolean isDeleted = false;
+}
