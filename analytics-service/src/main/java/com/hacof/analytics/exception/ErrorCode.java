cd3a76f60a9b27019e9d833f39fe79c08a171cf6
doanh <nguyentrongdoanh202@gmail.com>
2025-03-17 23:40:39 +0700

    define ErrorCode & GlobalExceptionHanler & FeedbackType

--- analytics-service/src/main/java/com/hacof/analytics/exception/ErrorCode.java
+++ analytics-service/src/main/java/com/hacof/analytics/exception/ErrorCode.java
@@ -7,7 +7,19 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    UNAUTHENTICATED(1016, "Unauthenticated", HttpStatus.UNAUTHORIZED);
+    UNCATEGORIZED(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    UNAUTHORIZED(1011, "You do not have permission", HttpStatus.FORBIDDEN),
+    UNAUTHENTICATED(1001, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    FEEDBACK_NOT_FOUND(1002, "Feedback not found", HttpStatus.NOT_FOUND),
+    FEEDBACK_EXISTED(1003, "Feedback existed", HttpStatus.BAD_REQUEST),
+    INVALID_FEEDBACK(1004, "Invalid feedback", HttpStatus.BAD_REQUEST),
+    INVALID_FEEDBACK_TYPE(1004, "Invalid feedback type", HttpStatus.BAD_REQUEST),
+    FEEDBACK_DETAIL_NOT_FOUND(1005, "Feedback detail not found", HttpStatus.NOT_FOUND),
+    FEEDBACK_DETAIL_ALREADY_EXISTS(1006, "Feedback detail already exists", HttpStatus.BAD_REQUEST),
+    TEAM_NOT_FOUND(1007, "Team not found", HttpStatus.NOT_FOUND),
+    HACKATHON_NOT_FOUND(1008, "Hackathon not found", HttpStatus.NOT_FOUND),
+    MENTOR_NOT_FOUND(1009, "Mentor not found", HttpStatus.NOT_FOUND),
+    INVALID_TARGET(1010, "Invalid target", HttpStatus.BAD_REQUEST);
 
     ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
