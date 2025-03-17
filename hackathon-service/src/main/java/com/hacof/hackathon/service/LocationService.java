58ea6f3aa6bcdb81c0c07e5b5510f8c164b8d534
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-18 03:18:55 +0700

    refactor code in location service

--- hackathon-service/src/main/java/com/hacof/hackathon/service/LocationService.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/service/LocationService.java
@@ -8,11 +8,11 @@ import com.hacof.hackathon.dto.LocationDTO;
 import com.hacof.hackathon.entity.Location;
 
 public interface LocationService {
-    List<LocationDTO> getAllLocations(Specification<Location> spec);
-
     LocationDTO createLocation(LocationDTO locationDTO);
 
     LocationDTO updateLocation(Long id, LocationDTO locationDTO);
 
     void deleteLocation(Long id);
+
+    List<LocationDTO> getLocations(Specification<Location> spec);
 }
