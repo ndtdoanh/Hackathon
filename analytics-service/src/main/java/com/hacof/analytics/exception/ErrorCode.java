5426e2c3b8305758141b0a49a9a831cb46aee588
doanh <nguyentrongdoanh202@gmail.com>
2025-03-18 00:47:19 +0700

    define ErrorCode for blogpost

--- analytics-service/src/main/java/com/hacof/analytics/exception/ErrorCode.java
+++ analytics-service/src/main/java/com/hacof/analytics/exception/ErrorCode.java
@@ -8,7 +8,6 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     UNCATEGORIZED(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
-    UNAUTHORIZED(1011, "You do not have permission", HttpStatus.FORBIDDEN),
     UNAUTHENTICATED(1001, "Unauthenticated", HttpStatus.UNAUTHORIZED),
     FEEDBACK_NOT_FOUND(1002, "Feedback not found", HttpStatus.NOT_FOUND),
     FEEDBACK_EXISTED(1003, "Feedback existed", HttpStatus.BAD_REQUEST),
@@ -19,7 +18,13 @@ public enum ErrorCode {
     TEAM_NOT_FOUND(1007, "Team not found", HttpStatus.NOT_FOUND),
     HACKATHON_NOT_FOUND(1008, "Hackathon not found", HttpStatus.NOT_FOUND),
     MENTOR_NOT_FOUND(1009, "Mentor not found", HttpStatus.NOT_FOUND),
-    INVALID_TARGET(1010, "Invalid target", HttpStatus.BAD_REQUEST);
+    INVALID_TARGET(1010, "Invalid target", HttpStatus.BAD_REQUEST),
+    UNAUTHORIZED(1011, "You do not have permission", HttpStatus.FORBIDDEN),
+    BLOG_NOT_FOUND(1012, "Blog post not found", HttpStatus.NOT_FOUND),
+    BLOG_CANNOT_SUBMIT(1013, "Only drafts can be submitted", HttpStatus.BAD_REQUEST),
+    BLOG_CANNOT_APPROVE(1014, "Only pending review blogs can be approved", HttpStatus.BAD_REQUEST),
+    BLOG_CANNOT_REJECT(1015, "Only pending review blogs can be rejected", HttpStatus.BAD_REQUEST),
+    BLOG_SLUG_ALREADY_EXISTS(1016, "Blog post slug already exists", HttpStatus.BAD_REQUEST);
 
     ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
