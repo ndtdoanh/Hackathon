f7fc244ad4ea87a374a617a4273badfa7f96aee4
doanh <nguyentrongdoanh202@gmail.com>
2025-03-19 04:26:54 +0700

    refactor ScheduleEventReminder for hackathon-service

--- hackathon-service/src/main/java/com/hacof/hackathon/entity/ScheduleEventReminder.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/entity/ScheduleEventReminder.java
@@ -2,9 +2,22 @@ package com.hacof.hackathon.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Entity
