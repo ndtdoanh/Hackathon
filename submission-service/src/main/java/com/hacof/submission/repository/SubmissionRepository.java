5f77c0202a1a404c3804e946c186f78ff33b4621
doanh <nguyentrongdoanh202@gmail.com>
2025-03-19 05:03:18 +0700

    modified SubmissionRepository for submission-service

--- submission-service/src/main/java/com/hacof/submission/repository/SubmissionRepository.java
+++ submission-service/src/main/java/com/hacof/submission/repository/SubmissionRepository.java
@@ -1,9 +1,9 @@
 package com.hacof.submission.repository;
 
-import com.hacof.submission.entity.Submission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.hacof.submission.entity.Submission;
+
 @Repository
-public interface SubmissionRepository extends JpaRepository<Submission, Long> {
-}
+public interface SubmissionRepository extends JpaRepository<Submission, Long> {}
