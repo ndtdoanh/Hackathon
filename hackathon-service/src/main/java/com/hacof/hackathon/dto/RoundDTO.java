f171e269a7c81d57f8403e876823859cd139ffb0
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-18 22:09:51 +0700

    refactor round dto

--- hackathon-service/src/main/java/com/hacof/hackathon/dto/RoundDTO.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/dto/RoundDTO.java
@@ -3,9 +3,6 @@ package com.hacof.hackathon.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import jakarta.validation.constraints.NotNull;
-
-import com.hacof.hackathon.constant.RoundName;
 import com.hacof.hackathon.entity.*;
 
 import lombok.Getter;
@@ -13,35 +10,17 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class RoundDTO extends AuditBaseDTO {
-    private Long id;
-
-    @NotNull(message = "Round name is mandatory (QUALIFYING, SEMIFINAL, FINAL)")
-    private RoundName roundName;
-
-    @NotNull(message = "Start date is mandatory")
-    private LocalDateTime startDate;
-
-    @NotNull(message = "End date is mandatory")
-    private LocalDateTime endDate;
-
-    String description;
-
-    @NotNull(message = "Max team is mandatory")
-    private int maxTeam;
-
-    @NotNull(message = "Is video round is mandatory")
-    private boolean isVideoRound = false;
-
-    private Long hackathonId;
-
-    List<Submission> submissions;
-
-    List<RoundMarkCriterion> roundMarkCriteria;
-
-    List<JudgeRound> judgeRounds;
-
-    List<TeamRound> teamRounds;
-
-    List<RoundLocation> roundLocations;
+public class RoundDTO {
+    private long id;
+    private long hackathonId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private int roundNumber;
+    private String roundTitle;
+    private String status;
+    private List<Long> submissionIds;
+    private List<Long> roundMarkCriterionIds;
+    private List<Long> judgeRoundIds;
+    private List<Long> teamRoundIds;
+    private List<Long> roundLocationIds;
 }
