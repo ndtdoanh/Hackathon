e151dfb3c435b9c5db4be95c07ddaaed24e7c002
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-18 22:02:22 +0700

    change Long id to long id

--- hackathon-service/src/main/java/com/hacof/hackathon/dto/EventDTO.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/dto/EventDTO.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EventDTO {
-    private Long id;
+    private long id;
     private String name;
     private String description;
     private LocalDateTime eventDate;
