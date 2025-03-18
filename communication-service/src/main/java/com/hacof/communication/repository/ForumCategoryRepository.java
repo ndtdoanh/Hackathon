86edfac86d2036e41975394744b7bd830d63d706
doanh <nguyentrongdoanh202@gmail.com>
2025-03-19 04:45:13 +0700

    refactor ForumCategoryRepository for communication-service

--- communication-service/src/main/java/com/hacof/communication/repository/ForumCategoryRepository.java
+++ communication-service/src/main/java/com/hacof/communication/repository/ForumCategoryRepository.java
@@ -1,9 +1,9 @@
 package com.hacof.communication.repository;
 
-import com.hacof.communication.entity.ForumCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.hacof.communication.entity.ForumCategory;
+
 @Repository
-public interface ForumCategoryRepository extends JpaRepository<ForumCategory, Long> {
-}
+public interface ForumCategoryRepository extends JpaRepository<ForumCategory, Long> {}
