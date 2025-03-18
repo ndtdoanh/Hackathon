4fdc3b16eafc3fb4e27166fd9316d11a0070e0e2
doanh <nguyentrongdoanh202@gmail.com>
2025-03-19 04:25:02 +0700

    refactor Permission for hackathon-service

--- hackathon-service/src/main/java/com/hacof/hackathon/entity/Permission.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/entity/Permission.java
@@ -3,9 +3,21 @@ package com.hacof.hackathon.entity;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Entity
