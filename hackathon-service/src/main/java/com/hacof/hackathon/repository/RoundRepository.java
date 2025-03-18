8d545bd223fdd139ad2e6855cd2670269ddf8075
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-18 22:10:08 +0700

    refactor round repository

--- hackathon-service/src/main/java/com/hacof/hackathon/repository/RoundRepository.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/repository/RoundRepository.java
@@ -1,12 +1,8 @@
 package com.hacof.hackathon.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.hacof.hackathon.entity.Round;
 
-public interface RoundRepository extends JpaRepository<Round, Long>, JpaSpecificationExecutor<Round> {
-    List<Round> findByHackathonId(Long hackathonId);
-}
+public interface RoundRepository extends JpaRepository<Round, Long>, JpaSpecificationExecutor<Round> {}
