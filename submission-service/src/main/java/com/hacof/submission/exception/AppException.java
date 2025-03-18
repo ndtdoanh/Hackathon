cab9a250140f8724e636009655a2abba2e272bfd
doanh <nguyentrongdoanh202@gmail.com>
2025-03-19 04:08:01 +0700

    add AppException for submission-service

---
+++ submission-service/src/main/java/com/hacof/submission/exception/AppException.java
@@ -0,0 +1,14 @@
+package com.hacof.submission.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppException extends RuntimeException {
+    private ErrorCode errorCode;
+}
