c3250bc2e6b9cfa92aa1feab478535b7a7c22fd5
doanh <nguyentrongdoanh202@gmail.com>
2025-03-19 04:22:10 +0700

    refactor InvalidatedToken for hackathon-service

--- hackathon-service/src/main/java/com/hacof/hackathon/entity/InvalidatedToken.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/entity/InvalidatedToken.java
@@ -6,7 +6,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Entity
