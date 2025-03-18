d6be5af182b81048d7f155a2f33d5a17d94b3dc8
doanh <nguyentrongdoanh202@gmail.com>
2025-03-19 04:17:12 +0700

    refactor Conversation for hackathon-service

--- hackathon-service/src/main/java/com/hacof/hackathon/entity/Conversation.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/entity/Conversation.java
@@ -3,11 +3,28 @@ package com.hacof.hackathon.entity;
 import java.util.List;
 import java.util.Set;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 import com.hacof.hackathon.constant.ConversationType;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Entity
