b977867b0de8e4ab04cd9c06f83e3def9a14d916
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-18 15:16:39 +0700

    add team service - team request, create team, invite member

--- hackathon-service/src/main/java/com/hacof/hackathon/service/TeamService.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/service/TeamService.java
@@ -2,14 +2,43 @@ package com.hacof.hackathon.service;
 
 import java.util.List;
 
+import org.springframework.data.jpa.domain.Specification;
+
 import com.hacof.hackathon.dto.TeamDTO;
+import com.hacof.hackathon.dto.TeamRequestDTO;
+import com.hacof.hackathon.dto.UserTeamDTO;
+import com.hacof.hackathon.dto.UserTeamRequestDTO;
+import com.hacof.hackathon.entity.Team;
 
 public interface TeamService {
-    TeamDTO createTeam(TeamDTO teamDTO, Long creatorId);
+    // Team Request Management
+    TeamRequestDTO createTeamRequest(TeamRequestDTO request);
 
-    TeamDTO inviteMemberToTeam(Long teamId, Long userId);
+    TeamRequestDTO approveTeamRequest(Long requestId, Long reviewerId);
 
-    TeamDTO requestToJoinTeam(Long teamId, Long userId);
+    TeamRequestDTO rejectTeamRequest(Long requestId, Long reviewerId);
 
-    List<TeamDTO> getAllTeams();
+    // Team Member Management
+    UserTeamRequestDTO inviteMember(Long teamId, Long userId, Long inviterId);
+
+    UserTeamRequestDTO acceptInvitation(Long requestId, Long userId);
+
+    UserTeamRequestDTO rejectInvitation(Long requestId, Long userId);
+
+    UserTeamRequestDTO requestToJoinTeam(Long teamId, Long userId);
+
+    UserTeamRequestDTO approveJoinRequest(Long requestId, Long leaderId);
+
+    UserTeamRequestDTO rejectJoinRequest(Long requestId, Long leaderId);
+
+    // Team Hackathon Registration
+    TeamDTO updateTeam(TeamDTO teamDTO);
+
+    TeamDTO getTeamById(Long id);
+
+    List<TeamDTO> getTeams(Specification<Team> spec);
+
+    void removeMember(UserTeamDTO memberDTO);
+
+    TeamDTO registerForHackathon(TeamDTO teamDTO);
 }
