964cd7944bd4a9a80e133062e2edaed7b158c77e
doanh <nguyentrongdoanh202@gmail.com>
2025-03-25 21:38:29 +0700

    modified TaskAssigneeMapper for communication-service

--- communication-service/src/main/java/com/hacof/communication/mapper/TaskAssigneeMapper.java
+++ communication-service/src/main/java/com/hacof/communication/mapper/TaskAssigneeMapper.java
@@ -1,21 +1,19 @@
 package com.hacof.communication.mapper;
 
+import org.springframework.stereotype.Component;
+
 import com.hacof.communication.dto.request.TaskAssigneeRequestDTO;
 import com.hacof.communication.dto.response.TaskAssigneeResponseDTO;
-import com.hacof.communication.entity.TaskAssignee;
 import com.hacof.communication.entity.Task;
+import com.hacof.communication.entity.TaskAssignee;
 import com.hacof.communication.entity.User;
-import org.springframework.stereotype.Component;
 
 @Component
 public class TaskAssigneeMapper {
 
     // Chuyển từ TaskAssigneeRequestDTO sang TaskAssignee entity
     public TaskAssignee toEntity(TaskAssigneeRequestDTO requestDTO, Task task, User user) {
-        return TaskAssignee.builder()
-                .task(task)
-                .user(user)
-                .build();
+        return TaskAssignee.builder().task(task).user(user).build();
     }
 
     // Chuyển từ TaskAssignee entity sang TaskAssigneeResponseDTO
@@ -23,9 +21,9 @@ public class TaskAssigneeMapper {
         return TaskAssigneeResponseDTO.builder()
                 .id(taskAssignee.getId())
                 .taskId(taskAssignee.getTask().getId())
-                .taskTitle(taskAssignee.getTask().getTitle())  // Giả sử Task có trường `title`
+                .taskTitle(taskAssignee.getTask().getTitle()) // Giả sử Task có trường `title`
                 .userId(taskAssignee.getUser().getId())
-                .userUsername(taskAssignee.getUser().getUsername())  // Giả sử User có trường `username`
+                .userUsername(taskAssignee.getUser().getUsername()) // Giả sử User có trường `username`
                 .createdDate(taskAssignee.getCreatedDate())
                 .lastModifiedDate(taskAssignee.getLastModifiedDate())
                 .build();
