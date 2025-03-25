8fdeafc9062b74be716619cac58f98c03615789b
doanh <nguyentrongdoanh202@gmail.com>
2025-03-25 21:42:24 +0700

    modified ScheduleEventReminderMapper for communication-service

--- communication-service/src/main/java/com/hacof/communication/mapper/ScheduleEventReminderMapper.java
+++ communication-service/src/main/java/com/hacof/communication/mapper/ScheduleEventReminderMapper.java
@@ -1,17 +1,19 @@
 package com.hacof.communication.mapper;
 
+import org.springframework.stereotype.Component;
+
 import com.hacof.communication.dto.request.ScheduleEventReminderRequestDTO;
 import com.hacof.communication.dto.response.ScheduleEventReminderResponseDTO;
-import com.hacof.communication.entity.ScheduleEventReminder;
 import com.hacof.communication.entity.ScheduleEvent;
+import com.hacof.communication.entity.ScheduleEventReminder;
 import com.hacof.communication.entity.User;
-import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduleEventReminderMapper {
 
     // Chuyển từ ScheduleEventReminderRequestDTO sang ScheduleEventReminder entity
-    public ScheduleEventReminder toEntity(ScheduleEventReminderRequestDTO requestDTO, ScheduleEvent scheduleEvent, User user) {
+    public ScheduleEventReminder toEntity(
+            ScheduleEventReminderRequestDTO requestDTO, ScheduleEvent scheduleEvent, User user) {
         return ScheduleEventReminder.builder()
                 .scheduleEvent(scheduleEvent)
                 .user(user)
