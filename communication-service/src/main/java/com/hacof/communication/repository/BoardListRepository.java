942c9ed6755d1851c12292d98281a6321b5c2e34
doanh <nguyentrongdoanh202@gmail.com>
2025-03-25 21:48:43 +0700

    modified BoardListRepository for communication-service

--- communication-service/src/main/java/com/hacof/communication/repository/BoardListRepository.java
+++ communication-service/src/main/java/com/hacof/communication/repository/BoardListRepository.java
@@ -1,9 +1,9 @@
 package com.hacof.communication.repository;
 
-import com.hacof.communication.entity.BoardList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.hacof.communication.entity.BoardList;
+
 @Repository
-public interface BoardListRepository extends JpaRepository<BoardList, Long> {
-}
+public interface BoardListRepository extends JpaRepository<BoardList, Long> {}
