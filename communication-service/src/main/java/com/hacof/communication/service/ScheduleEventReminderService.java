ea82addf7864ee5cd862ddb4b130b0572e2a35f3
doanh <nguyentrongdoanh202@gmail.com>
2025-03-25 21:41:39 +0700

    modified ScheduleEventReminderService for communication-service

--- communication-service/src/main/java/com/hacof/communication/service/ScheduleEventReminderService.java
+++ communication-service/src/main/java/com/hacof/communication/service/ScheduleEventReminderService.java
@@ -1,15 +1,17 @@
 package com.hacof.communication.service;
 
+import java.util.List;
+
 import com.hacof.communication.dto.request.ScheduleEventReminderRequestDTO;
 import com.hacof.communication.dto.response.ScheduleEventReminderResponseDTO;
 
-import java.util.List;
-
 public interface ScheduleEventReminderService {
 
-    ScheduleEventReminderResponseDTO createScheduleEventReminder(ScheduleEventReminderRequestDTO scheduleEventReminderRequestDTO);
+    ScheduleEventReminderResponseDTO createScheduleEventReminder(
+            ScheduleEventReminderRequestDTO scheduleEventReminderRequestDTO);
 
-    ScheduleEventReminderResponseDTO updateScheduleEventReminder(Long id, ScheduleEventReminderRequestDTO scheduleEventReminderRequestDTO);
+    ScheduleEventReminderResponseDTO updateScheduleEventReminder(
+            Long id, ScheduleEventReminderRequestDTO scheduleEventReminderRequestDTO);
 
     void deleteScheduleEventReminder(Long id);
 
