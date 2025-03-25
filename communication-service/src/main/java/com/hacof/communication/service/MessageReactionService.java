8fe8812b6d9af8d084adb59cc9d5473da3958d6c
doanh <nguyentrongdoanh202@gmail.com>
2025-03-26 00:20:36 +0700

    modified MessageReactionService for communication-service

--- communication-service/src/main/java/com/hacof/communication/service/MessageReactionService.java
+++ communication-service/src/main/java/com/hacof/communication/service/MessageReactionService.java
@@ -1,9 +1,14 @@
 package com.hacof.communication.service;
 
+import java.util.List;
+
 import com.hacof.communication.dto.request.MessageReactionRequest;
+import com.hacof.communication.dto.response.MessageReactionResponse;
 
 public interface MessageReactionService {
-    void addReaction(Long messageId, MessageReactionRequest request);
+    MessageReactionResponse reactToMessage(Long messageId, MessageReactionRequest request);
 
     void removeReaction(Long reactionId);
+
+    List<MessageReactionResponse> getReactionsByMessage(Long messageId);
 }
