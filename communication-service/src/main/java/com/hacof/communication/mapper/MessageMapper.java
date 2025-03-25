23f19866b8371b1befff7f473d8f7266be26b1f1
doanh <nguyentrongdoanh202@gmail.com>
2025-03-26 00:22:29 +0700

    modified MessageMapper for communication-service

--- communication-service/src/main/java/com/hacof/communication/mapper/MessageMapper.java
+++ communication-service/src/main/java/com/hacof/communication/mapper/MessageMapper.java
@@ -1,17 +1,23 @@
 package com.hacof.communication.mapper;
 
-import java.util.List;
-
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
+import com.hacof.communication.dto.request.MessageCreateRequest;
+import com.hacof.communication.dto.request.MessageUpdateRequest;
 import com.hacof.communication.dto.response.MessageResponse;
 import com.hacof.communication.entity.Message;
 
 @Mapper(componentModel = "spring")
 public interface MessageMapper {
-    @Mapping(source = "createdBy.id", target = "createdBy")
-    MessageResponse messageToResponse(Message message);
+    @Mapping(target = "createdByUserId", source = "createdBy.id")
+    @Mapping(target = "conversationId", source = "conversation.id")
+    MessageResponse toMessageResponse(Message message);
 
-    List<MessageResponse> messagesToResponses(List<Message> messages);
+    @Mapping(target = "id", ignore = true)
+    Message toMessage(MessageCreateRequest request);
+
+    @Mapping(target = "id", ignore = true)
+    void updateMessageFromRequest(MessageUpdateRequest request, @MappingTarget Message message);
 }
