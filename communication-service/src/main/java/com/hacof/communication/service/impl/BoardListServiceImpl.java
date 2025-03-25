347cc7b1b43513de8ff6365165a94bb332f51b88
doanh <nguyentrongdoanh202@gmail.com>
2025-03-25 21:48:58 +0700

    modified BoardListServiceImpl for communication-service

--- communication-service/src/main/java/com/hacof/communication/service/impl/BoardListServiceImpl.java
+++ communication-service/src/main/java/com/hacof/communication/service/impl/BoardListServiceImpl.java
@@ -1,5 +1,12 @@
 package com.hacof.communication.service.impl;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.hacof.communication.dto.request.BoardListRequestDTO;
 import com.hacof.communication.dto.response.BoardListResponseDTO;
 import com.hacof.communication.entity.Board;
@@ -8,12 +15,6 @@ import com.hacof.communication.mapper.BoardListMapper;
 import com.hacof.communication.repository.BoardListRepository;
 import com.hacof.communication.repository.BoardRepository;
 import com.hacof.communication.service.BoardListService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class BoardListServiceImpl implements BoardListService {
