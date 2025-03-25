1cd7ac0ba3313228841ec41e614db1400cda22e0
doanh <nguyentrongdoanh202@gmail.com>
2025-03-26 00:22:50 +0700

    add FileUrlMapper for communication-service

---
+++ communication-service/src/main/java/com/hacof/communication/mapper/FileUrlMapper.java
@@ -0,0 +1,24 @@
+package com.hacof.communication.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.hacof.communication.dto.response.FileUrlResponse;
+import com.hacof.communication.entity.FileUrl;
+
+@Mapper(componentModel = "spring")
+public interface FileUrlMapper {
+    FileUrlMapper INSTANCE = Mappers.getMapper(FileUrlMapper.class);
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "fileName", source = "fileName")
+    @Mapping(target = "fileUrl", source = "fileUrl")
+    @Mapping(target = "fileType", source = "fileType")
+    @Mapping(target = "fileSize", source = "fileSize")
+    FileUrlResponse toResponse(FileUrl fileUrl);
+
+    List<FileUrlResponse> toResponseList(List<FileUrl> fileUrls);
+}
