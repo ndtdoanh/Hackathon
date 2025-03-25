1e6b360ff44d912e4013c34b839037089b219aee
doanh <nguyentrongdoanh202@gmail.com>
2025-03-25 21:47:35 +0700

    modified BoardLabelController for communication-service

--- communication-service/src/main/java/com/hacof/communication/controller/BoardLabelController.java
+++ communication-service/src/main/java/com/hacof/communication/controller/BoardLabelController.java
@@ -1,15 +1,16 @@
 package com.hacof.communication.controller;
 
-import com.hacof.communication.dto.request.BoardLabelRequestDTO;
-import com.hacof.communication.dto.response.BoardLabelResponseDTO;
-import com.hacof.communication.response.CommonResponse;
-import com.hacof.communication.service.BoardLabelService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import com.hacof.communication.dto.request.BoardLabelRequestDTO;
+import com.hacof.communication.dto.response.BoardLabelResponseDTO;
+import com.hacof.communication.response.CommonResponse;
+import com.hacof.communication.service.BoardLabelService;
 
 @RestController
 @RequestMapping("/api/v1/board-labels")
