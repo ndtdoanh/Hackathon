c58a7af38f41953b87eb9bcd11d7eb11b328e0d2
doanh <nguyentrongdoanh202@gmail.com>
2025-03-25 21:39:46 +0700

    modified ScheduleRequestDTO for communication-service

--- communication-service/src/main/java/com/hacof/communication/dto/request/ScheduleRequestDTO.java
+++ communication-service/src/main/java/com/hacof/communication/dto/request/ScheduleRequestDTO.java
@@ -9,7 +9,7 @@ import lombok.*;
 @AllArgsConstructor
 public class ScheduleRequestDTO {
 
-    private Long teamId;  // ID của Team liên kết với Schedule
-    private String name;  // Tên của Schedule
+    private Long teamId; // ID của Team liên kết với Schedule
+    private String name; // Tên của Schedule
     private String description; // Mô tả về Schedule
 }
