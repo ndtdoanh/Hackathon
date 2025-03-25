085b37f09663671cfc91c5fcb2f7384049fccf6e
doanh <nguyentrongdoanh202@gmail.com>
2025-03-25 21:38:43 +0700

    modified TaskAssigneeResponseDTO for communication-service

--- communication-service/src/main/java/com/hacof/communication/dto/response/TaskAssigneeResponseDTO.java
+++ communication-service/src/main/java/com/hacof/communication/dto/response/TaskAssigneeResponseDTO.java
@@ -11,11 +11,11 @@ import lombok.*;
 @AllArgsConstructor
 public class TaskAssigneeResponseDTO {
 
-    private Long id;            // ID của TaskAssignee
-    private Long taskId;        // ID của Task mà TaskAssignee này thuộc về
-    private String taskTitle;   // Tiêu đề của Task mà TaskAssignee này thuộc về
-    private Long userId;        // ID của User mà TaskAssignee này liên kết
+    private Long id; // ID của TaskAssignee
+    private Long taskId; // ID của Task mà TaskAssignee này thuộc về
+    private String taskTitle; // Tiêu đề của Task mà TaskAssignee này thuộc về
+    private Long userId; // ID của User mà TaskAssignee này liên kết
     private String userUsername; // Username của User mà TaskAssignee này liên kết
-    private LocalDateTime createdDate;  // Thời gian tạo TaskAssignee
+    private LocalDateTime createdDate; // Thời gian tạo TaskAssignee
     private LocalDateTime lastModifiedDate; // Thời gian sửa đổi TaskAssignee
 }
