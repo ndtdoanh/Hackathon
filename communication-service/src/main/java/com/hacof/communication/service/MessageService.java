9347620a12b44726efec97cc22fc9083639f4830
doanh <nguyentrongdoanh202@gmail.com>
2025-03-26 00:19:08 +0700

    modified MessageService for communication-service

--- communication-service/src/main/java/com/hacof/communication/service/MessageService.java
+++ communication-service/src/main/java/com/hacof/communication/service/MessageService.java
@@ -1,5 +1,7 @@
 package com.hacof.communication.service;
 
+import java.util.List;
+
 import com.hacof.communication.dto.request.MessageCreateRequest;
 import com.hacof.communication.dto.request.MessageUpdateRequest;
 import com.hacof.communication.dto.response.MessageResponse;
@@ -7,7 +9,9 @@ import com.hacof.communication.dto.response.MessageResponse;
 public interface MessageService {
     MessageResponse createMessage(Long conversationId, MessageCreateRequest request);
 
-    MessageResponse updateMessage(Long id, MessageUpdateRequest request);
+    List<MessageResponse> getMessagesByConversation(Long conversationId);
 
-    void deleteMessage(Long id);
+    MessageResponse updateMessage(Long messageId, MessageUpdateRequest request);
+
+    void deleteMessage(Long messageId);
 }
