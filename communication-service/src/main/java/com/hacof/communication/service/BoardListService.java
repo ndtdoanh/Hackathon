c628042d4d61201782b8f588417134fb8ae282d9
doanh <nguyentrongdoanh202@gmail.com>
2025-03-25 21:48:52 +0700

    modified BoardListService for communication-service

--- communication-service/src/main/java/com/hacof/communication/service/BoardListService.java
+++ communication-service/src/main/java/com/hacof/communication/service/BoardListService.java
@@ -1,9 +1,9 @@
 package com.hacof.communication.service;
 
+import java.util.List;
+
 import com.hacof.communication.dto.request.BoardListRequestDTO;
 import com.hacof.communication.dto.response.BoardListResponseDTO;
-
-import java.util.List;
 
 public interface BoardListService {
 
