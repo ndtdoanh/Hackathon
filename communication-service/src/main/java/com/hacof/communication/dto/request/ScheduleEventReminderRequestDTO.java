73b3309fea5c9b8c09465787623dc6e9023c7a11
doanh <nguyentrongdoanh202@gmail.com>
2025-03-25 21:42:05 +0700

    modified ScheduleEventReminderRequestDTO for communication-service

--- communication-service/src/main/java/com/hacof/communication/dto/request/ScheduleEventReminderRequestDTO.java
+++ communication-service/src/main/java/com/hacof/communication/dto/request/ScheduleEventReminderRequestDTO.java
@@ -11,7 +11,7 @@ import lombok.*;
 @AllArgsConstructor
 public class ScheduleEventReminderRequestDTO {
 
-    private Long scheduleEventId;  // ID của ScheduleEvent mà Reminder này thuộc về
-    private Long userId;           // ID của User nhận Reminder
+    private Long scheduleEventId; // ID của ScheduleEvent mà Reminder này thuộc về
+    private Long userId; // ID của User nhận Reminder
     private LocalDateTime remindAt; // Thời gian nhắc nhở
 }
