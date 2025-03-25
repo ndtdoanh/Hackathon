8f9a3d443e543c1320dc7b5fc578470a216511c2
doanh <nguyentrongdoanh202@gmail.com>
2025-03-25 21:52:41 +0700

    modified ErrorCode for communication-service

--- communication-service/src/main/java/com/hacof/communication/exception/ErrorCode.java
+++ communication-service/src/main/java/com/hacof/communication/exception/ErrorCode.java
@@ -7,7 +7,28 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    UNAUTHENTICATED(1016, "Unauthenticated", HttpStatus.UNAUTHORIZED);
+    UNCATEGORIZED(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    UNAUTHENTICATED(1001, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    USER_NOT_EXISTED(1002, "User not existed", HttpStatus.NOT_FOUND),
+    UNAUTHORIZED(1003, "You do not have permission", HttpStatus.FORBIDDEN),
+
+    INVALID_CONVERSATION_REQUEST(1004, "Invalid conversation request", HttpStatus.BAD_REQUEST),
+    INVALID_GROUP_CONVERSATION_REQUEST(1005, "Invalid group conversation request", HttpStatus.BAD_REQUEST),
+    CANNOT_CREATE_CONVERSATION_WITH_SELF(1006, "Cannot create conversation with self", HttpStatus.BAD_REQUEST),
+    NOT_CONVERSATION_CREATOR(1007, "Not conversation creator", HttpStatus.BAD_REQUEST),
+
+    CONVERSATION_NOT_EXISTED(1008, "Conversation not found", HttpStatus.NOT_FOUND),
+    USER_NOT_IN_CONVERSATION(1009, "Users are not in the conversation", HttpStatus.FORBIDDEN),
+
+    INVALID_REQUEST(1010, "Invalid request", HttpStatus.BAD_REQUEST),
+    CONVERSATION_ALREADY_EXISTS(1011, "Conversation already exist", HttpStatus.BAD_REQUEST),
+
+    CANNOT_ADD_USER_TO_SINGLE_CONVERSATION(1012, "Cannot add user to single conversation", HttpStatus.BAD_REQUEST),
+    CANNOT_UPDATE_SINGLE_CONVERSATION(1013, "Cannot update single conversation", HttpStatus.BAD_REQUEST),
+    CANNOT_REMOVE_USER_FROM_SINGLE_CONVERSATION(
+            1014, "Cannot remove user from single conversation", HttpStatus.BAD_REQUEST),
+
+    USER_ALREADY_IN_CONVERSATION(1015, "User already in conversation", HttpStatus.BAD_REQUEST);
 
     ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
