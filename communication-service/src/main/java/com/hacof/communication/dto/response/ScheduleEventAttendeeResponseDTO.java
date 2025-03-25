8fa22caa057b5d682ff6d21458602333e65ebe7d
doanh <nguyentrongdoanh202@gmail.com>
2025-03-25 21:44:04 +0700

    modified ScheduleEventAttendeeResponseDTO for communication-service

--- communication-service/src/main/java/com/hacof/communication/dto/response/ScheduleEventAttendeeResponseDTO.java
+++ communication-service/src/main/java/com/hacof/communication/dto/response/ScheduleEventAttendeeResponseDTO.java
@@ -1,7 +1,9 @@
 package com.hacof.communication.dto.response;
 
-import com.hacof.communication.constant.ScheduleEventStatus;
 import java.time.LocalDateTime;
+
+import com.hacof.communication.constant.ScheduleEventStatus;
+
 import lombok.*;
 
 @Getter
@@ -11,12 +13,12 @@ import lombok.*;
 @AllArgsConstructor
 public class ScheduleEventAttendeeResponseDTO {
 
-    private Long id;              // ID của ScheduleEventAttendee
+    private Long id; // ID của ScheduleEventAttendee
     private Long scheduleEventId; // ID của ScheduleEvent mà Attendee này tham gia
-    private Long userId;          // ID của User tham gia
+    private Long userId; // ID của User tham gia
     private ScheduleEventStatus status; // Trạng thái tham gia
 
     // Trường AuditBase
-    private LocalDateTime createdDate;  // Thời gian tạo
+    private LocalDateTime createdDate; // Thời gian tạo
     private LocalDateTime lastModifiedDate; // Thời gian sửa đổi
 }
