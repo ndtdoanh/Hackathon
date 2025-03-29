39e2e9e585d3922b7b7bbf51d2a7222316cb266f
lqhuy28 <lequanghuy2809@gmail.com>
2025-03-29 16:59:47 +0700

    add RoleResponse

---
+++ submission-service/src/main/java/com/hacof/submission/dto/response/RoleResponse.java
@@ -0,0 +1,26 @@
+package com.hacof.submission.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RoleResponse {
+    long id;
+    String name;
+    String description;
+    LocalDateTime createdDate;
+    LocalDateTime lastModifiedDate;
+    Long createdByUserId;
+    Set<PermissionResponse> permissions;
+}
