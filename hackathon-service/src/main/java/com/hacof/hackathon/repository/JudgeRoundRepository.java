10150aa02f64b376955cde2433d35615f8bd38db
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-29 19:44:05 +0700

    remove judge round components in hackathon-service : controller, dto, mapper, repository, service implementation

--- hackathon-service/src/main/java/com/hacof/hackathon/repository/JudgeRoundRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hacof.hackathon.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hacof.hackathon.entity.JudgeRound;
-
-public interface JudgeRoundRepository extends JpaRepository<JudgeRound, Long> {}
--- hackathon-service/src/main/java/com/hacof/hackathon/service/JudgeRoundService.java
+++
@@ -1,7 +0,0 @@
-package com.hacof.hackathon.service;
-
-import com.hacof.hackathon.dto.JudgeRoundDTO;
-
-public interface JudgeRoundService {
-    JudgeRoundDTO assignJudgeToRound(Long judgeId, Long roundId);
-}
