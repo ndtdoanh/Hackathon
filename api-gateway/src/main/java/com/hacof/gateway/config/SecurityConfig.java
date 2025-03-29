fe324ad1443db326cbf8ac66506e1d95604663bc
doanh <nguyentrongdoanh202@gmail.com>
2025-03-29 14:24:03 +0700

    update cors for api-gateway

--- api-gateway/src/main/java/com/hacof/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/hacof/gateway/config/SecurityConfig.java
@@ -1,22 +1,27 @@
 package com.hacof.gateway.config;
 
+import org.springframework.cloud.gateway.config.GlobalCorsProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsWebFilter corsWebFilter(GlobalCorsProperties globalCorsProperties) {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
+
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+        config.setAllowCredentials(false);
+
+        source.registerCorsConfiguration("/api/**", config);
+
+        return new CorsWebFilter(source);
     }
 }
