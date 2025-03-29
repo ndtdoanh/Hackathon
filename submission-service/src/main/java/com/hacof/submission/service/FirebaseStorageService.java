47046a956cd5e815c4732851b42dfa471013c965
lqhuy28 <lequanghuy2809@gmail.com>
2025-03-30 04:23:59 +0700

    Refactor code

--- submission-service/src/main/java/com/hacof/submission/service/FirebaseStorageService.java
+++
@@ -1,9 +0,0 @@
-package com.hacof.submission.service;
-
-import java.io.IOException;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FirebaseStorageService {
-    String uploadFile(MultipartFile file) throws IOException;
-}
