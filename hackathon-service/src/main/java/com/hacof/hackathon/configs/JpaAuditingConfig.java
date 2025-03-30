393ddc003fe14e2816711572dca4928acfa16462
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-30 20:51:38 +0700

    remove unuse dto

---
+++ hackathon-service/src/main/java/com/hacof/hackathon/configs/JpaAuditingConfig.java
@@ -0,0 +1,11 @@
+package com.hacof.hackathon.configs;
+
+//// @Configuration
+//// @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+// public class JpaAuditingConfig {
+//
+//    @Bean
+//    public AuditorAware<User> auditorAware(UserRepository userRepository) {
+//        return new AuditorAwareImpl(userRepository);
+//    }
+// }
--- hackathon-service/src/main/java/com/hacof/hackathon/dto/JoinRequest.java
+++
@@ -1,9 +0,0 @@
-package com.hacof.hackathon.dto;
-
-import lombok.Data;
-
-@Data
-public class JoinRequest {
-    private Long teamId;
-    private Long userId;
-}
