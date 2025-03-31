cfba68540624cb7d657c4e9385acd41c2167b553
lqhuy28 <lequanghuy2809@gmail.com>
2025-03-31 11:52:50 +0700

    Update request of JudgeSubmission

--- submission-service/src/main/java/com/hacof/submission/dto/request/JudgeSubmissionRequestDTO.java
+++ submission-service/src/main/java/com/hacof/submission/dto/request/JudgeSubmissionRequestDTO.java
@@ -3,10 +3,14 @@ package com.hacof.submission.dto.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class JudgeSubmissionRequestDTO {
     private Long judgeId;
     private Long submissionId;
+    private int score;
     private String note;
+    private List<JudgeSubmissionDetailRequestDTO> judgeSubmissionDetails;
 }
