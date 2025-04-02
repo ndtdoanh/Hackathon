7241589c1c8a2e9e730781bf026d864d5640fa13
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-04-02 07:28:28 +0700

    remove unuse class

--- hackathon-service/src/main/java/com/hacof/hackathon/dto/ApproveRejectRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.hacof.hackathon.dto;
-
-import lombok.Data;
-
-@Data
-public class ApproveRejectRequestDTO {
-    private long teamRequestId;
-    private long userId;
-}
--- hackathon-service/src/main/java/com/hacof/hackathon/service/IdentityClientService.java
+++
@@ -1,9 +0,0 @@
-package com.hacof.hackathon.service;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "identity-service")
-public interface IdentityClientService {
-    //    @GetMapping("/api/v1/users")
-    //    ApiResponse<List<UserResponse>> getUsers(@RequestHeader("Authorization") String token);
-}
