8791fc12e8701836a2cf89ab6eacd5ea43b9a20c
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-04-08 21:31:57 +0700

    search team

--- hackathon-service/src/main/java/com/hacof/hackathon/specification/TeamSpecification.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/specification/TeamSpecification.java
@@ -1,5 +1,9 @@
 package com.hacof.hackathon.specification;
 
+import com.hacof.hackathon.entity.TeamHackathon;
+import com.hacof.hackathon.entity.UserTeam;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -35,14 +39,32 @@ public class TeamSpecification {
                 criteriaBuilder.equal(root.get("teamLeader").get("id"), leaderId);
     }
 
-    // Filter teams by hackathonId
-    public static Specification<Team> hasHackathonId(Long hackathonId) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(
-                root.join("teamHackathons").get("hackathon").get("id"), hackathonId);
-    }
+//    // Filter teams by hackathonId
+//    public static Specification<Team> hasHackathonId(Long hackathonId) {
+//        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(
+//                root.join("teamHackathons").get("hackathon").get("id"), hackathonId);
+//    }
 
     // Combine the two filters: by leaderId and hackathonId
-    public static Specification<Team> hasLeaderIdAndHackathonId(Long leaderId, Long hackathonId) {
-        return Specification.where(hasLeaderId(leaderId)).and(hasHackathonId(hackathonId));
+//    public static Specification<Team> hasLeaderIdAndHackathonId(Long leaderId, Long hackathonId) {
+//        return Specification.where(hasLeaderId(leaderId)).and(hasHackathonId(hackathonId));
+//    }
+
+    public static Specification<Team> hasUserIdInMembers(Long userId) {
+        return (root, query, cb) -> {
+            Join<Team, UserTeam> members = root.join("teamMembers", JoinType.INNER);
+            return cb.equal(members.get("user").get("id"), userId);
+        };
+    }
+
+    public static Specification<Team> hasHackathonId(Long hackathonId) {
+        return (root, query, cb) -> {
+            Join<Team, TeamHackathon> hackathons = root.join("teamHackathons", JoinType.INNER);
+            return cb.equal(hackathons.get("hackathon").get("id"), hackathonId);
+        };
+    }
+
+    public static Specification<Team> hasUserIdAndHackathonId(Long userId, Long hackathonId) {
+        return Specification.where(hasUserIdInMembers(userId)).and(hasHackathonId(hackathonId));
     }
 }
