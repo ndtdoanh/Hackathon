d95d2f13b9325c7dbd1ee0526a503765bfc1f66f
doanh <nguyentrongdoanh202@gmail.com>
2025-04-11 21:24:42 +0700

    add interceptor

--- communication-service/src/main/java/com/hacof/communication/config/WebSocketConfig.java
+++ communication-service/src/main/java/com/hacof/communication/config/WebSocketConfig.java
@@ -1,6 +1,10 @@
 package com.hacof.communication.config;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,7 +13,12 @@ import org.springframework.web.socket.config.annotation.WebSocketTransportRegist
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    WebSocketChannelInterceptor webSocketChannelInterceptor;
+    WebSocketHandshakeInterceptor webSocketHandshakeInterceptor;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -20,7 +29,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:3000")
+                .addInterceptors(webSocketHandshakeInterceptor)
+                .withSockJS();
     }
 
     @Override
@@ -29,5 +41,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setMessageSizeLimit(8192)
                 .setSendBufferSizeLimit(512 * 1024)
                 .setSendTimeLimit(20 * 1000);
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(webSocketChannelInterceptor);
     }
 }
