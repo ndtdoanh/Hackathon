52365b4d6957205ba1ed15bc176a184182232fc5
doanh <nguyentrongdoanh202@gmail.com>
2025-04-18 04:28:42 +0700

    add rate limiting middleware with redis

--- api-gateway/src/main/java/com/hacof/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/hacof/gateway/ApiGatewayApplication.java
@@ -3,6 +3,9 @@ package com.hacof.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,5 +13,10 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
     }
 }
