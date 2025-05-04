2de1e84b421f6c07ea2410d5596cee96d6f4cbe7
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-05-04 16:37:10 +0700

    handle null parse deserialized

--- identity-service/src/main/java/com/hacof/identity/dto/CustomLocalDateTimeDeserialized.java
+++ identity-service/src/main/java/com/hacof/identity/dto/CustomLocalDateTimeDeserialized.java
@@ -5,17 +5,24 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
 public class CustomLocalDateTimeDeserialized extends JsonDeserializer<LocalDateTime> {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
+    //    @Override
+    //    public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt)
+    //            throws IOException, JsonProcessingException {
+    //        String date = p.getText();
+    //        return LocalDateTime.parse(date, formatter);
+    //    }
     @Override
-    public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt)
-            throws IOException, JsonProcessingException {
+    public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
         String date = p.getText();
+        if (date.contains(".")) {
+            date = date.substring(0, date.indexOf("."));
+        }
         return LocalDateTime.parse(date, formatter);
     }
 }
