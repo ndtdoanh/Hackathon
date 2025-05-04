3763db06f9f5fdf4175c7f0e9324b03152e3d10b
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-05-04 19:15:43 +0700

    remove fc

--- hackathon-service/src/main/java/com/hacof/hackathon/service/IdentityClientService.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/service/IdentityClientService.java
@@ -1,31 +1,33 @@
-package com.hacof.hackathon.service;
-
-import com.hacof.identity.dto.ApiResponse;
-import com.hacof.identity.dto.response.UserResponse;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-
-import java.util.List;
-
-@FeignClient(name = "identity-service", path = "/api/v1/users")
-public interface IdentityClientService {
-    @GetMapping
-    ApiResponse<List<UserResponse>> getUsers(@RequestHeader("Authorization") String token);
-
-    @GetMapping("/{userId}")
-    ApiResponse<UserResponse> getUserById(@RequestHeader("Authorization") String token, @PathVariable String userId);
-
-    @GetMapping("/username/{username}")
-    ApiResponse<UserResponse> getUserByUsername(@RequestHeader("Authorization") String token, @PathVariable String username);
-
-    @GetMapping("/users-by-roles")
-    ApiResponse<List<UserResponse>> getUsersByRoles(@RequestHeader("Authorization") String token);
-
-    @GetMapping("/team-members")
-    ApiResponse<List<UserResponse>> getTeamMembers(@RequestHeader("Authorization") String token);
-
-    @GetMapping("/users-by-created-by/{createdByUserName}")
-    ApiResponse<List<UserResponse>> getUsersByCreatedByUserName(@RequestHeader("Authorization") String token, @PathVariable String createdByUserName);
-}
+// package com.hacof.hackathon.service;
+//
+// import com.hacof.identity.dto.ApiResponse;
+// import com.hacof.identity.dto.response.UserResponse;
+// import org.springframework.cloud.openfeign.FeignClient;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RequestHeader;
+//
+// import java.util.List;
+//
+// @FeignClient(name = "identity-service", path = "/api/v1/users")
+// public interface IdentityClientService {
+//    @GetMapping
+//    ApiResponse<List<UserResponse>> getUsers(@RequestHeader("Authorization") String token);
+//
+//    @GetMapping("/{userId}")
+//    ApiResponse<UserResponse> getUserById(@RequestHeader("Authorization") String token, @PathVariable String userId);
+//
+//    @GetMapping("/username/{username}")
+//    ApiResponse<UserResponse> getUserByUsername(@RequestHeader("Authorization") String token, @PathVariable String
+// username);
+//
+//    @GetMapping("/users-by-roles")
+//    ApiResponse<List<UserResponse>> getUsersByRoles(@RequestHeader("Authorization") String token);
+//
+//    @GetMapping("/team-members")
+//    ApiResponse<List<UserResponse>> getTeamMembers(@RequestHeader("Authorization") String token);
+//
+//    @GetMapping("/users-by-created-by/{createdByUserName}")
+//    ApiResponse<List<UserResponse>> getUsersByCreatedByUserName(@RequestHeader("Authorization") String token,
+// @PathVariable String createdByUserName);
+// }
