8d2a090d6c2fd416b4f35f1060d1f12999e02454
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-05-04 16:40:21 +0700

    use feign client to communication with identity-service from hackathon-service

---
+++ hackathon-service/src/main/java/com/hacof/hackathon/controller/IdentityController.java
@@ -0,0 +1,50 @@
+package com.hacof.hackathon.controller;
+
+import com.hacof.hackathon.service.IdentityClientService;
+import com.hacof.identity.dto.ApiResponse;
+import com.hacof.identity.dto.response.UserResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/identity")
+@RequiredArgsConstructor
+@Slf4j
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
+public class IdentityController {
+    IdentityClientService identityClientService;
+
+    @GetMapping("/users")
+    public ApiResponse<List<UserResponse>> getUsers(@RequestHeader("Authorization") String token) {
+        return identityClientService.getUsers(token);
+    }
+
+    @GetMapping("/users/{userId}")
+    public ApiResponse<UserResponse> getUserById(@RequestHeader("Authorization") String token, @PathVariable String userId) {
+        return identityClientService.getUserById(token, userId);
+    }
+
+    @GetMapping("/users/username/{username}")
+    public ApiResponse<UserResponse> getUserByUsername(@RequestHeader("Authorization") String token, @PathVariable String username) {
+        return identityClientService.getUserByUsername(token, username);
+    }
+
+    @GetMapping("/users-by-roles")
+    public ApiResponse<List<UserResponse>> getUsersByRoles(@RequestHeader("Authorization") String token) {
+        return identityClientService.getUsersByRoles(token);
+    }
+
+    @GetMapping("/team-members")
+    public ApiResponse<List<UserResponse>> getTeamMembers(@RequestHeader("Authorization") String token) {
+        return identityClientService.getTeamMembers(token);
+    }
+
+    @GetMapping("/users-by-created-by/{createdByUserName}")
+    public ApiResponse<List<UserResponse>> getUsersByCreatedByUserName(@RequestHeader("Authorization") String token, @PathVariable String createdByUserName) {
+        return identityClientService.getUsersByCreatedByUserName(token, createdByUserName);
+    }
+}
--- hackathon-service/src/main/java/com/hacof/hackathon/service/impl/IdentityClientServiceImpl.java
+++ hackathon-service/src/main/java/com/hacof/hackathon/service/impl/IdentityClientServiceImpl.java
@@ -1,20 +1,51 @@
 package com.hacof.hackathon.service.impl;
 
+import com.hacof.hackathon.service.IdentityClientService;
+import com.hacof.identity.dto.ApiResponse;
+import com.hacof.identity.dto.response.UserResponse;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 @Slf4j
-public class IdentityClientServiceImpl {
-    //    private final IdentityClientService identityClientService;
-    //
-    //    @GetMapping("/users")
-    //    public ApiResponse<List<UserResponse>> getUsers(@RequestHeader("Authorization") String token) {
-    //        log.info("Calling identity-service with token: {}", token);
-    //        return identityClientService.getUsers(token);
-    //    }
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
+public class IdentityClientServiceImpl implements IdentityClientService {
 
+    IdentityClientService identityClient;
+
+    @Override
+    public ApiResponse<List<UserResponse>> getUsers(String token) {
+        return identityClient.getUsers(token);
+    }
+
+    @Override
+    public ApiResponse<UserResponse> getUserById(String token, String userId) {
+        return identityClient.getUserById(token, userId);
+    }
+
+    @Override
+    public ApiResponse<UserResponse> getUserByUsername(String token, String username) {
+        return identityClient.getUserByUsername(token, username);
+    }
+
+    @Override
+    public ApiResponse<List<UserResponse>> getUsersByRoles(String token) {
+        return identityClient.getUsersByRoles(token);
+    }
+
+    @Override
+    public ApiResponse<List<UserResponse>> getTeamMembers(String token) {
+        return identityClient.getTeamMembers(token);
+    }
+
+    @Override
+    public ApiResponse<List<UserResponse>> getUsersByCreatedByUserName(String token, String createdByUserName) {
+        return identityClient.getUsersByCreatedByUserName(token, createdByUserName);
+    }
 }
